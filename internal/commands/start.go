72f877ce19e18152fcf3dd50f5ba2d50ee918687
OMNIX OTT <owner.omnix@gmail.com>
2026-03-14 16:48:59 +0530

    Update start.go

--- internal/commands/start.go
+++ internal/commands/start.go
@@ -26,6 +26,6 @@ func start(ctx *ext.Context, u *ext.Update) error {
 		ctx.Reply(u, "You are not allowed to use this bot.", nil)
 		return dispatcher.EndGroups
 	}
-	ctx.Reply(u, "Hi, send me any file to get a direct streamble link to that file.", nil)
+	ctx.Reply(u, "👋 Welcome User!\n\n📤 Send your file to get a\n🔗 Direct Streamable / Download Link from OMNIX Cloud.\n\n🔔 Join our Back-Up Group:\nhttps://t.me/omnixempire", nil)
 	return dispatcher.EndGroups
 }
